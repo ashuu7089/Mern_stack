6d34db91f6042cd5d81f1e84df3934bcdfd0cda6
Ashu pateriya <107784054+ashuu7089@users.noreply.github.com>
2025-03-24 21:48:31 +0530

    Create userRouter.js

---
+++ src/routers/userRouter.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const multer = require('multer');
+const upload = multer()
+const userController = require('../controllers/userController');
+const authMiddleware = require('../middlewares/authorization');
+
+router.post('/register', upload.none(), userController.createUserRegisterAPI);
+router.post('/login', upload.none(), userController.userLoginAPI);
+router.get('/', authMiddleware, userController.getUsers);
+
+module.exports = router;
