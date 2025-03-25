a1a55e3854521bd862d24e9fcf3d76ed386a9968
Ashu pateriya <107784054+ashuu7089@users.noreply.github.com>
2025-03-25 08:35:46 +0530

    Update appointmentRouter.js

--- src/routers/appointmentRouter.js
+++ src/routers/appointmentRouter.js
@@ -6,6 +6,7 @@ const authMiddleware = require("../middlewares/authorization");
 
 router.post("/", authMiddleware, appointment.createAppointmentAPI);
 router.get("/", authMiddleware, appointment.getAppointments);
+router.get("/:doctorId", appointment.getAppointmentsByDoctorId);
 router.delete("/cancel/:id", authMiddleware, appointment.cancelAppointment);
 
 module.exports = router;
